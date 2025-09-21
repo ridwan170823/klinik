0687806afc8755680ec8aff3ea2e8a10142b47c2
ridwan170823 <ridwan1ef@gmail.com>
2025-09-21 21:13:52 +0700

    a

--- resources/views/admin/laporan/index.blade.php
+++ resources/views/admin/laporan/index.blade.php
@@ -113,7 +113,7 @@
           <div class="card-body">
             @forelse ($services as $service)
               <div class="d-flex justify-content-between align-items-center py-2 border-bottom small">
-                <span>{{ optional($service->layanan)->nama ?? 'Layanan tidak diketahui' }}</span>
+               <span>{{ data_get($service, 'layanan.nama', 'Layanan tidak diketahui') }}</span>
                 <span class="badge badge-primary">{{ $service->total }}</span>
               </div>
             @empty
